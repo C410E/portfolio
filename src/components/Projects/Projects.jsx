d5fcc685ca0a670ba5a8fb64e58ed7f27bb5d771
caioe <caioevandro0@gmail.com>
2023-09-11 09:22:22 -0300

    atualizações

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import "./Projects.css";
-//colocar a api dogithub aqui
+
 const Projects = () => {
     const [itemsApi, setItemsApi] = useState([]);
 
